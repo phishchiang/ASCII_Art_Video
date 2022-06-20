76f6cf0ddedab2fb558c783c9e55498bc056b803
phishchiang <phishchiang@gmail.com>
2022-06-20 21:17:29 +0800

    Removed extra lines.

--- js/app.js
+++ js/app.js
@@ -80,11 +80,6 @@ export default class Sketch {
     this.material_vid.uniforms.canvas.value = this.mc;
     this.mc.needsUpdate = true;
 
-    // this.c1 = document.getElementById('c1');
-    // this.ctx1 = this.c1.getContext('2d');
-    // this.c2 = document.getElementById('c2');
-    // this.ctx2 = this.c2.getContext('2d');
-
     this.video.addEventListener('play', () => {
         // this.width = video.videoWidth / 2;
         // this.height = video.videoHeight / 2;
